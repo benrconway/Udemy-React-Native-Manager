44d98ce842bba06a4c9b445b41531a591026c9f2
Ben Conway <benrconway84@gmail.com>
2019-10-31 10:51:04 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <Text>Manager App</Text>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
