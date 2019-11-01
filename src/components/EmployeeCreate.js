6100a20e3d0fb984ddbef7197e1b48c150757a70
Ben Conway <benrconway84@gmail.com>
2019-11-01 11:34:56 +0000

    Added AuthActions, EmployeeActions and associated addition of the reducer

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return(
-      <View>
-        <Text>EmployeeCreate</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+          label="Name"
+          placeholder="Jane"
+          />
+          <Input
+          label="Phone #"
+          placeholder="555-555-5555"
+          />
+        </CardSection>
+        <CardSection>
+
+        </CardSection>
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
     );
   };
 }
